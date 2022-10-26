b679320039a92cfeeca18807ce3c791e5ee75616
Mayra Tejada <44760892+MayraTejada@users.noreply.github.com>
2022-10-25 23:34:09 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     #Database URL
-    SQLALCHEMY_DATABASE_URI =  'mysql://admin:Redeban2022*@database-1.cvfo0tjysyng.us-east-1.rds.amazonaws.com:3306/mydatabasehaus'
+    SQLALCHEMY_DATABASE_URI =  'mysql:/mydatabasehaus'
 
 
 class DevelopmentConfig(Config):
